1e3f0c3396dd2c1a3436da9435463d6b2dd4871f
Amooh005 <amoskipkoech2005@gmail.com>
2025-03-19 05:40:07 +0300

    week 2

---
+++ day-2.py
@@ -0,0 +1,10 @@
+import random
+students_age=[23,43,32,43]
+print(students_age[2])
+k=random.choice(students_age)
+print(k)
+my_list=["w","e","l","w","e"]
+print(my_list[0:2])
+numbers=[1,4,3,5,5,3,3]
+numbers.append(8)
+print(numbers)
---
+++ stark.py
@@ -0,0 +1,6 @@
+stark=[]
+stark.append(3)
+stark.append(5)
+stark.append(8)
+print(stark.pop())
+print(stark.remove())
